bfbeb755734ee24ce76772976d13fa69deb70bf5
Alex Vasile <48962821+alex-vasile@users.noreply.github.com>
2023-08-14 21:36:43 -0400

    Updated testing suite

--- features/steps/consistency.py
+++ features/steps/consistency.py
@@ -3,13 +3,22 @@ import os.path
 import subprocess
 import os
 import time
+from behave import *
 
 
 def get_dist_name():
-    found_executable = os.getenv('DIST_LOC', 'dist_location_not_set')
-    print(found_executable)  # print statements are only executed by behave
-    # when the behavior fails, so this print is usually silenced if all is well
-    return found_executable
+    release_exe = "../cmake-build-release/x3f_extract"
+    debug_exe   = "../cmake-build-debug/x3f_extract"
+    if os.path.exists(release_exe):
+        executable = release_exe
+    elif os.path.exists(debug_exe):
+        executable = debug_exe
+    else:
+        raise IOError("Couldn't find x3f_extract")
+    # print statements are only executed by behave when the behavior fails,
+    # so this print is usually silenced if all is well
+    print(executable)
+    return executable
 
 
 def run_conversion(args):
@@ -17,14 +26,14 @@ def run_conversion(args):
     running_proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # suppressing output
     while running_proc.poll() is None:
         time.sleep(0.1)
-    assert running_proc.returncode is 0
+    assert running_proc.returncode == 0
 
 
 @given(u'an input image {image} without a {converted_image}')
 def step_impl(context, image, converted_image):
     assert os.path.isfile(image)
     if os.path.isfile(converted_image):
-        os.chmod(converted_image, 0666)
+        os.chmod(converted_image, 0o666)
         os.remove(converted_image)
 
 
@@ -62,6 +71,7 @@ def step_impl(context, image):
 def step_impl(context, image, output_format):
     found_executable = get_dist_name()
     args = ['-color', 'none']
+    print(output_format)
     if output_format == 'UNPROCESSED':
         args = args + ['-unprocessed']
     if output_format == 'QTOP':
@@ -78,14 +88,21 @@ def step_impl(context, image, output_format):
     args = [found_executable] + args + [image]
     run_conversion(args)
 
+
 @then(u'the {converted_image} has the right {md5} hash value')
 def step_impl(context, converted_image, md5):
     assert os.path.isfile(converted_image)
-    with open(converted_image) as ci:
-        found_hash = hashlib.md5(ci.read()).hexdigest()
-        print("found_hash: ", found_hash, " expected_hash: ", md5)
-        assert md5 == found_hash
-    os.chmod(converted_image, 0666)
+    md5_found = hashlib.md5()
+    with open(converted_image, 'rb') as file:
+        # Read ing 4K chunks rather than loading whole file in memory.
+        # Costs ~5% is speed, but significantly reduces memory requirements.
+        for block in iter(lambda: file.read(4096), b""):
+            md5_found.update(block)
+    md5_found = md5_found.hexdigest()
+    if md5_found != md5:
+        print("found_hash: ", md5_found, " expected_hash: ", md5)
+        assert False
+    os.chmod(converted_image, 0o666)
     os.remove(converted_image)  # normally, I'd remove this file in the environment
     # however, if these files should always be removed, then remove them immediately after
     # the test should be sufficient.  This should be the last 'then' statement
